8aeb27ee1a35d0b9a59b83ca16b1b282b871d48d
madan <reddymadan0512#gmail.com>
2023-06-04 22:56:41 +0530

    AD

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const gameBoard = document.getElementById("gameboard");
 const infoDisplay = document.getElementById("info");
 const startCells = ["", "", "", "", "", "", "", "", ""];
+
+let go = "circle";
+infoDisplay.textContent = "circle goes first";
+
 function createBoard() {
   startCells.forEach((_cell, index) => {
     const cellElement = document.createElement("div");
@@ -10,7 +14,45 @@ function createBoard() {
     gameBoard.append(cellElement);
   });
 }
+createBoard();
 
 function addGo(e) {
-  console.log(e.target);
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  e.target.append(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "it is now" + go + "s go.";
+  e.target.removeEventListener("click", addGo);
+  checkScore();
+}
+function checkScore() {
+  const allScore = document.querySelectorAll(".square");
+  // console.log(allScore);
+  const winningCombs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  winningCombs.forEach((array) => {
+    const circleWins = array.every((cell) => allScore[cell].firstChild?.classList.contains("circle"));
+    if (circleWins) {
+      infoDisplay.textContent = "circle wins";
+      allScore.forEach((square) => square.replaceWith(square.cloneNode(true)));
+      return;
+    }
+  });
+
+  winningCombs.forEach((array) => {
+    const crossWins = array.every((cell) => allScore[cell].firstChild?.classList.contains("cross"));
+    if (crossWins) {
+      infoDisplay.textContent = "cross wins";
+      allScore.forEach((square) => square.replaceWith(square.cloneNode(true)));
+      return;
+    }
+  });
 }
